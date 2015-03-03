5758b94e2f6a2200a8dd486346eb1b7765fc578f
upulj <jay.upul@gmail.com>
2015-03-03 17:50:46 +1100

    upulj mob-work1 for ga-mob-class
    upulj mob-work1 for ga-mob-class

---
+++ Profile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Upul Jayalath on 2/03/2015.
+//  Copyright (c) 2015 Mine Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindFromSegue(segue: UIStoryboardSegue) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
